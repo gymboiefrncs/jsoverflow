6dff917cb80c2186963e9e1307e69f3a706b3b15
francis <dominiquefrancis42@gmail.com>
2025-12-07 13:00:12 +0800

    fix: changed authController to controller

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import * as authController from "../controllers/authController.js";
+import * as controller from "../controllers/authController.js";
 import { validateLogin, validateUser } from "../validators/authValidator.js";
 const router = express.Router();
 
-router.post("/signup", validateUser, authController.signUp);
-router.post("/login", validateLogin, authController.login);
-router.delete("/logout", authController.logout);
+router.post("/signup", validateUser, controller.signUp);
+router.post("/login", validateLogin, controller.login);
+router.delete("/logout", controller.logout);
 export default router;
